7ed789d3b2187e8e22c0fbd85646c02d6ee102ff
Sixten Paulsson <sixtenpaulsson@gmail.com>
2025-01-31 10:51:57 +0100

    kommentarer för saab och volvo

--- Saab95.java
+++ Saab95.java
@@ -1,22 +1,26 @@
 import java.awt.*;
 
 public class Saab95 extends Car {
-
+    //Varia
     private boolean turboOn;
 
+    //Konstruktor
     public Saab95() {
         super(2, 125, Color.red, "Saab95");
         turboOn = false;
         stopEngine();
 
     }
+    //Sätter på turbo
     public void setTurboOn() {
         turboOn = true;
     }
+    //Stänger av turbo
     public void setTurboOff() {
         turboOn = false;
     }
-
+    //Saab95s speedfactor funktion
+    @Override
     public double speedFactor() {
         double turbo = 1;
         if(turboOn) turbo = 1.3;
--- Volvo240.java
+++ Volvo240.java
@@ -4,11 +4,13 @@ public class Volvo240 extends Car{
 
     private final static double trimFactor = 1.25;
 
-    
+    //Konstruktor
     public Volvo240(){
         super(4,100,Color.black,"Volvo240");
         stopEngine();
     }
+    //Volvo240s speedfactor funkton
+    @Override
     public double speedFactor(){
         return getEnginePower() * 0.01 * trimFactor;
     }
