8750d0d505e5f757608d17d0f4e97dfcb2b2188e
filiplarsen231 <filiplarsen231@gmail.com>
2025-02-05 11:54:52 +0100

    l

--- Scania.java
+++ Scania.java
@@ -8,7 +8,7 @@ public class Scania extends Car{
         super(2,120,Color.white,"Scania R500 V8 XT 6x4 Euro 6");
     }
 
-    @Override // lastbilar accelererar långsamt detta bör ändrass
+    @Override // lastbilar accelererar långsamt detta bör ändras
     public double speedFactor() {
         return getEnginePower() * 0.01;
     }
